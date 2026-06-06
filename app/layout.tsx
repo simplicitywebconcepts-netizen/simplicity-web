a1c9933a9688625a1589ac2e6a01583e9e871e95
Krishnakant Rai <raikrishna365@gmail.com>
2026-06-06 16:32:42 +0530

    fix: logo size

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,6 +47,11 @@ export const metadata: Metadata = {
                 type: "image/svg+xml",
             },
             {
+                url: "/favicon-96x96.png",
+                sizes: "96x96",
+                type: "image/png",
+            },
+            {
                 url: "/web-app-manifest-192x192.png",
                 sizes: "192x192",
                 type: "image/png",
