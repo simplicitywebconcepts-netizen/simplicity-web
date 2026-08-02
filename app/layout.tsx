34b87604ad113ad15a4ed95a3f15e968303faa67
Krishnakant Rai <raikrishna365@gmail.com>
2026-08-02 12:02:12 +0530

    perf: defer Google Tag Manager script to afterInteractive

--- app/layout.tsx
+++ app/layout.tsx
@@ -112,7 +112,7 @@ export default function RootLayout({
                 {/* Google Tag Manager */}
                 <Script
                     id="gtm-script"
-                    strategy="beforeInteractive"
+                    strategy="afterInteractive"
                     dangerouslySetInnerHTML={{
                         __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
